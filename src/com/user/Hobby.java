0b8e4010ca420a89c421036a345bf68fcd793739
21900662 <63633539+21900662@users.noreply.github.com>
2020-08-13 05:28:23 +0900

    8.4_hw4

---
+++ src/com/user/Hobby.java
@@ -0,0 +1,13 @@
+package com.user;
+
+public class Hobby {
+	private String[] hobby;
+
+	public String[] getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String[] hobby) {
+		this.hobby = hobby;
+	}
+}
